b1b92eef96bd3116b3d310831df5eacb7a472011
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-07-10 17:38:28 +0300

    Add init file

---
+++ aioalice/__init__.py
@@ -0,0 +1,13 @@
+import asyncio
+
+# from .dispatcher import Dispatcher
+
+try:
+    import uvloop
+except ImportError:
+    pass
+else:
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+__version__ = '0.1.0'
