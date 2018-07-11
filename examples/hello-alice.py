69eebb5e1eed9f47b84fd04581e99fccc2ce511d
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-07-11 13:10:34 +0300

    Create hello world example

---
+++ examples/hello-alice.py
@@ -0,0 +1,26 @@
+import logging
+
+from aiohttp import web
+from aioalice import Dispatcher, get_new_configured_app
+
+
+WEBHOOK_URL_PATH = '/my-alice-webhook/'  # webhook endpoint
+
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 3001
+
+
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
+
+dp = Dispatcher()
+
+
+@dp.request_handler()
+async def handle_all_requests(alice_request):
+    return alice_request.response('Привет этому миру!')
+
+
+if __name__ == '__main__':
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
