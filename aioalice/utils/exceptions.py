6c42cde69e745789b62aa10217cbd02c45267372
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-07-19 22:54:35 +0300

    Create exceptions

---
+++ aioalice/utils/exceptions.py
@@ -0,0 +1,38 @@
+class DialogsAPIError(Exception):
+    '''Base Exception for API related requests'''
+    __subclasses = []
+    match = None
+
+    def __init_subclass__(cls, match=None, **kwargs):
+        super(DialogsAPIError, cls).__init_subclass__(**kwargs)
+        if match is not None:
+            cls.match = match.lower()
+            cls.__subclasses.append(cls)
+
+    @classmethod
+    def detect(cls, description):
+        """Detect API Error (match by response text)"""
+        description = description.strip()
+        match = description.lower()
+        for err in cls.__subclasses:
+            if err is cls:
+                pass
+            if err.match in match:
+                raise err(description)
+        raise cls(description)
+
+
+class NetworkError(DialogsAPIError):
+    '''Is thrown when aiohttp client throws an error'''
+
+
+# class ClientError(DialogsAPIError):
+#     '''Is thrown when response code is 4xx'''
+
+
+# class ServerError(DialogsAPIError):
+#     '''Is thrown when response code is 5xx'''
+
+
+class Forbidden(DialogsAPIError, match='Forbidden'):
+    '''Is thrown when Authorization failed (403)'''
