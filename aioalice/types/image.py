ed2b16b8d9f07273352041d7ce65edc85e474e7b
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-07-24 20:55:21 +0300

    Create Image class

---
+++ aioalice/types/image.py
@@ -0,0 +1,13 @@
+from attr import attrs, attrib
+
+from . import AliceObject, MediaButton
+from aioalice.utils import ensure_cls
+
+
+@attrs
+class Image(AliceObject):
+    """Image object"""
+    image_id = attrib(type=str)
+    title = attrib(type=str)
+    description = attrib(type=str)
+    button = attrib(convert=ensure_cls(MediaButton))
