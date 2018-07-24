5f28a08ca11587c8bcdfd3bd04a47ab6e8bcc038
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-07-24 21:55:50 +0300

    Create Card class

--- aioalice/types/card_header.py
+++ aioalice/types/card_header.py
@@ -4,5 +4,5 @@ from . import AliceObject
 
 @attrs
 class CardHeader(AliceObject):
-    """This object represents a card's footer"""
+    """This object represents a card's header"""
     text = attrib(type=str)
