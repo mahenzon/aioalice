8a303371c2bb4d08381d6746da2b44f67f2042df
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-10-04 22:21:02 +0300

    Optimize safe_kwargs

--- aioalice/utils/safe_kwargs.py
+++ aioalice/utils/safe_kwargs.py
@@ -4,18 +4,13 @@ import inspect
 import functools
 
 
-def check_spec(func: callable, kwargs: dict):
-    spec = inspect.getfullargspec(func)
-    if spec.varkw:
-        return kwargs
-
-    return {k: v for k, v in kwargs.items() if k in spec.args}
-
-
 def safe_kwargs(func_or_class):
+    spec = inspect.getfullargspec(func_or_class)
+    all_args = spec.args
+
     @functools.wraps(func_or_class)
     def wrap(*args, **kwargs):
-        spec = check_spec(func_or_class, kwargs)
-        return func_or_class(*args, **spec)
+        accepted_kwargs = {k: v for k, v in kwargs.items() if k in all_args}
+        return func_or_class(*args, **accepted_kwargs)
 
     return wrap
