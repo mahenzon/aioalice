c2c4e3fc1c49d3c0158bb5cbc4192a42568d4d14
uburuntu <bekbulatov.ramzan@ya.ru>
2022-04-10 15:16:28 +0100

    new: support Yandex's side states
    https://yandex.ru/dev/dialogs/alice/doc/session-persistence.html

--- aioalice/types/alice_response.py
+++ aioalice/types/alice_response.py
@@ -10,4 +10,7 @@ class AliceResponse(AliceObject):
 
     response = attrib(converter=ensure_cls(Response))
     session = attrib(converter=ensure_cls(BaseSession))
+    session_state = attrib(type=dict)
+    user_state_update = attrib(type=dict)
+    application_state = attrib(type=dict)
     version = attrib(type=str)
